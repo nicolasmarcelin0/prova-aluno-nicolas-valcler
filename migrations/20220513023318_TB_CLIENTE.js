41463bf07b5e5ef9c228d32c8e97d563117716a5
Nicolas Marcelino <nicolasmarcelinomarcelino@yahoo.com.br>
2022-05-13 00:33:04 -0300

    Migrations

---
+++ migrations/20220513023318_TB_CLIENTE.js
@@ -0,0 +1,17 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+ exports.up = function(knex, Promise) {
+    return knex.schema.createTable('TB_CLIENTE', table => {
+        table.increments('id').primary()
+        table.integer('codigo').unique().notNull()
+        table.string('cnpjcpf').unique()
+        table.string('nome')
+            .notNull().defaultTo(true)
+    })
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTable('TB_CLIENTE')
+};
