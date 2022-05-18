645d060cfe632ca5e55fd725851bdbe20203f4c7
Nicolas Marcelino <nicolas.marcelino.mota08@aluno.ifce.edu.br>
2022-05-18 15:23:44 -0300

    Alt.

--- seeds/03-nota_fiscal.js
+++ seeds/03-nota_fiscal.js
@@ -12,22 +12,21 @@ exports.seed = async function(knex) {
       codigo: 35263,
       data: '13/02/2022',
       valorNota: 18.99,
-      cliente_id: 5,
+      cliente_id: 11,
       itens_id: 1
-      
     },
     {
       codigo: 65146,
       data: '14/02/2022',
       valorNota: 16.99,
-      cliente_id: 6,
+      cliente_id: 12,
       itens_id: 2
     },
     {
       codigo: 34865,
       data: '15/02/2022',
       valorNota: 14.99,
-      cliente_id: 7,
+      cliente_id: 13,
       itens_id: 3
     }
   ]);
