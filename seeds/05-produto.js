66ad68bb5c7c91ce8e32d7fb9854025e34f12570
Nicolas Marcelino <nicolas.marcelino.mota08@aluno.ifce.edu.br>
2022-05-25 15:20:28 -0300

    .insert e .update em TB_PRODUTO

--- seeds/05-produto.js
+++ seeds/05-produto.js
@@ -4,14 +4,11 @@
  */
 
 exports.seed = async function(knex) {
-  // Deletes ALL existing entries
   await knex('TB_PRODUTO').insert([{codigo: 1111, descricao: 'ALFACE', valor: 3.40}]);
-
   await knex('TB_PRODUTO')
   .where('descricao', '=', 'SUCRILHOS')
   .update({
     valor: 6.5
   })
-
 };
 
