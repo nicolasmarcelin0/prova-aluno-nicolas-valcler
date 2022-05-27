b93728cc0b7ef739fcec5460ed24a3a0e10b4985
Nicolas Marcelino <nicolasmarcelinomarcelino@yahoo.com.br>
2022-05-27 18:29:16 -0300

    CRUD aplicado em NOTA_FISCAL_VENDA

--- seeds/08-item_nota_fiscal.js
+++ seeds/08-item_nota_fiscal.js
@@ -3,11 +3,5 @@
  * @returns { Promise<void> } 
  */
 exports.seed = async function(knex) {
-  // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
-  ]);
+
 };
